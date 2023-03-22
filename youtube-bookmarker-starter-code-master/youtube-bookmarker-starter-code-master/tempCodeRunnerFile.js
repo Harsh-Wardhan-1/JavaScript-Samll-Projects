ea30517e6926522e771d4b2b60d0d8d0a5dded5b
Harsh Wardhan <harsh1wardhan1@gmail.com>
2023-03-22 15:46:15 +0530

    remove-and-add

--- youtube-bookmarker-starter-code-master/youtube-bookmarker-starter-code-master/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-if (tab.url && tab.url.includes("youtube.com/watch")) {
-    const queryParameters = tab.url.split("?")[1];
-    const urlParameters = new URLSearchParams(queryParameters);
-    console.log(urlParameters)
